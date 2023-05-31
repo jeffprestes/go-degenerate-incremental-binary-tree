a5915edba8425be772295aa329ee757c93d4e776
Jeff Prestes <jeffprestes@gmail.com>
2023-05-31 14:42:42 -0300

    feat: test tree with zero value

--- degenerate_test.go
+++ degenerate_test.go
@@ -16,3 +16,12 @@ func TestNewTree(t *testing.T) {
 	assert.NotNil(t, tree.ActualRoot)
 	assert.Equal(t, tree.ActualRoot.Text(10), "5199363853932272446084541931873785938987820779897294035064941545455873932186")
 }
+
+func TestTreeWithZero(t *testing.T) {
+	tree := NewDegenerateAndIncrementalBinaryTree(big.NewInt(0))
+	assert.NotNil(t, tree)
+	assert.Nil(t, tree.ActualRoot)
+	tree.InsertLeaf(big.NewInt(0))
+	assert.NotNil(t, tree.ActualRoot)
+	t.Log(tree.ActualRoot.Text(10))
+}
